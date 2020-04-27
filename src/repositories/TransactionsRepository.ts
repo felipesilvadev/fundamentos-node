a181bc86ac69433a85c84babb4e411429d836032
Felipe Silva <felipe.salaberga@gmail.com>
2020-04-27 20:21:43 -0300

    Challenge concluded

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,36 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
+  }
+
+  private calculateBalance(type: string): number {
+    return this.transactions
+      .filter(transaction => transaction.type === type)
+      .reduce((result, item) => result + item.value, 0);
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.calculateBalance('income');
+    const outcome = this.calculateBalance('outcome');
+    const total = income - outcome;
+
+    const balance: Balance = { income, outcome, total };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    const { total } = this.getBalance();
+    if (type === 'outcome' && value > total) {
+      throw Error('Cannot create outcome transaction without a valid balance');
+    }
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
